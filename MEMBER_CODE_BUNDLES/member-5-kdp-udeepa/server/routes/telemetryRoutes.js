ee4653aa63f7573d4ae30eae49875f625a163c58
ImanshaAGK13 <imanshakumudesh991@gmail.com>
2026-08-29 17:30:53 +0530

    chore: remove MEMBER_CODE_BUNDLES and MEMBER_CODE_BUNDLES.zip from repository

--- MEMBER_CODE_BUNDLES/member-5-kdp-udeepa/server/routes/telemetryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getTelemetryStats } from '../controllers/telemetryController.js';
-
-const router = express.Router();
-
-router.get('/stats', getTelemetryStats);
-
-export default router;
